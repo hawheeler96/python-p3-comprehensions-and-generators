20fcbf915ed15c52cf9fe66e38c7e8a068d88159
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-12-11 12:11:55 -0800

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
+print(return_evens([0,1,2,3,4,5,6,7,8,9,10]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [f"{phrase}!" for phrase in sentence_list]
+    return exclamations
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
